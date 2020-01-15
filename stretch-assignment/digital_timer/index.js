1ba21232cce90809ef7aa88fee0879abf48c3eb9
David Horstman <davidzilla27@gmail.com>
2020-01-15 18:39:28 -0500

    Stretch done - timer stops at 10s and turns red.

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,9 +1,14 @@
 let time = 0;
-function count(){
-    time++;
-    document.getElementById('msTens').textContent=time % 10;
-    document.getElementById('msHundreds').textContent = Math.floor(time/10) % 10;
-    document.getElementById('secondOnes').textContent = Math.floor(time/100) % 10;
-    document.getElementById('secondTens').textContent = Math.floor(time/1000) % 10;
+function count() {
+    if (time < 1000) {
+        time++;
+        document.getElementById('msTens').textContent = time % 10;
+        document.getElementById('msHundreds').textContent = Math.floor(time / 10) % 10;
+        document.getElementById('secondOnes').textContent = Math.floor(time / 100) % 10;
+        document.getElementById('secondTens').textContent = Math.floor(time / 1000) % 10;
+    }
+    else {
+        document.querySelectorAll('.digit').forEach(x=>x.style.color="red");
+    }
 }
 window.setInterval(count, 10);
