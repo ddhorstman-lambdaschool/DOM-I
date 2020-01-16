87070f0c3f65c4ec871aa293eb77683f622550ec
David Horstman <davidzilla27@gmail.com>
2020-01-15 19:03:37 -0500

    Stretch: Add reset button.

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,5 +1,10 @@
 let time = 0;
+//I'm sure there's a way to cancel out setInterval, but
+//I don't know it and have no internet access :/
+let timerRunning = false;
+window.setInterval(count, 10);
 function count() {
+    if(!timerRunning) return;
     if (time < 1000) {
         time++;
         document.getElementById('msTens').textContent = time % 10;
@@ -8,13 +13,22 @@ function count() {
         document.getElementById('secondTens').textContent = Math.floor(time / 1000) % 10;
     }
     else {
+        timerRunning = false;
         document.querySelector('.digits').classList.add('redDigit');
-        document.getElementById('start-button').removeAttribute("disabled");
-
     }
 }
 function startTimer(){
-    time = 0;
-    window.setInterval(count, 10);
+    timerRunning = true;
     document.getElementById('start-button').setAttribute("disabled","");
+    document.getElementById('reset-button').removeAttribute("disabled");
+}
+function resetTimer(){
+    timerRunning = false;
+    time = 0;
+    document.querySelector('.digits').classList.remove('redDigit');
+    document.getElementById('start-button').removeAttribute("disabled");
+    document.getElementById('reset-button').setAttribute("disabled","");
+    document.querySelectorAll('.digit').forEach(x => {
+        if(x.id!=='colon')x.textContent = '-';
+    });
 }
