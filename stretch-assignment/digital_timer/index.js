34a62f2be211d308bb310cdcb66a4a6f8f493d07
David Horstman <davidzilla27@gmail.com>
2020-01-15 22:48:37 -0800

    Removed superfluous Math.floor.

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -6,9 +6,9 @@ function count() {
     if (time < maxTime) {
         time++;
         document.getElementById('msTens').textContent = time % 10;
-        document.getElementById('msHundreds').textContent = Math.floor(time / 10) % 10;
-        document.getElementById('secondOnes').textContent = Math.floor(time / 100) % 10;
-        document.getElementById('secondTens').textContent = Math.floor(time / 1000) % 10;
+        document.getElementById('msHundreds').textContent = (time / 10) % 10;
+        document.getElementById('secondOnes').textContent = (time / 100) % 10;
+        document.getElementById('secondTens').textContent = (time / 1000) % 10;
     }
     else {
         clearInterval(timerID);
